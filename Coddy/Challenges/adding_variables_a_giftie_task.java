b4edf27176a408ef295b0eadeae33c325671c90b
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-12 23:06:20 -0300

    Exercícios e desafios do Coddy

---
+++ Coddy/Challenges/adding_variables_a_giftie_task.java
@@ -0,0 +1,17 @@
+// Inicialize duas variáveis int x e y com os valores 5 e 10. 
+// Em seguida, crie outra variável int result para armazenar a soma delas.
+
+
+class adding_variables_a_giftie_task {
+    public static void main(String[] args) {
+        // Write code here
+        int x = 5;
+        int y = 10;
+        int result = x + y;
+        
+        // Don't change below this line
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("result = " + result);
+    }
+}
---
+++ Coddy/Challenges/gallflower_greetings.java
@@ -0,0 +1,10 @@
+// Escreva um programa que inicialize uma variável String com "Greetings, Gallflower!" e a imprima.
+
+class gallflower_greetings {
+    public static void main(String[] args) {
+        // Write code here
+        String greeting = "Greetings, Gallflower!";
+        System.out.println(greeting);
+        // Don't change below this line
+    }
+}
