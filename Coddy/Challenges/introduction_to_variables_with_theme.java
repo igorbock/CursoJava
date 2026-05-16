2f9651ccc10d6434a104ad93942cfa0f091bc902
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-15 23:21:26 -0300

    Desafio e exercícios de Java

---
+++ Coddy/Challenges/introduction_to_variables_with_theme.java
@@ -0,0 +1,16 @@
+// Inicialize as seguintes variáveis:
+
+// us: String com o valor "United States"
+// population: int com o valor 331002651
+
+public class introduction_to_variables_with_theme {
+     public static void main(String[] args) {
+        // Write code here
+        String us = "United States";
+        int population = 331002651;
+
+        // Don't change below this line
+        System.out.println("us = " + us);
+        System.out.println("population = " + population);
+    }
+}
