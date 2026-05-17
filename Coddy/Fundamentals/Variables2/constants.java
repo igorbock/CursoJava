4e46c7f477e0a5026460bedfc26bc3dcc1de2a26
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-17 12:20:18 -0300

    Exercícios de Java, no Coddy

---
+++ Coddy/Fundamentals/Variables2/constants.java
@@ -0,0 +1,11 @@
+// Crie uma constante chamada PI e inicialize-a com o valor 3.14159.
+
+public class constants {
+    public static void main(String[] args) {
+        // Type your code below
+        final double PI = 3.14159;
+
+        // Don't change the line below
+        System.out.println("PI = " + PI);
+    }
+}
