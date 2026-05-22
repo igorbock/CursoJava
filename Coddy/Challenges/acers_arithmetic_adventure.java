37fbcbf0dfdbbd3b6df3957a2de9e8801767df5c
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-21 23:08:16 -0300

    Desafio de Java

---
+++ Coddy/Challenges/acers_arithmetic_adventure.java
@@ -0,0 +1,22 @@
+// Inicialize duas int variáveis num1 e num2 com os valores 7 e 4. 
+// Além disso, inicialize variáveis para sua sum, difference, product e quotient.
+
+public class acers_arithmetic_adventure{
+    public static void main(String[] args) {
+        // Write code here
+        int num1 = 7;
+        int num2 = 4;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        int quotient = num1 / num2;
+        
+        // Don’t change below this line
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        System.out.println("sum = " + sum);
+        System.out.println("difference = " + difference);
+        System.out.println("product = " + product);
+        System.out.println("quotient = " + quotient);
+    }
+}
