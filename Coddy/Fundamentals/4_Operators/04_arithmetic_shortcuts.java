da2d0d13708ec6ac6c88b15b49898d0cb0546aa1
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-23 21:40:53 -0300

    Subindo exercícios de Python

---
+++ Coddy/Fundamentals/4_Operators/04_arithmetic_shortcuts.java
@@ -0,0 +1,13 @@
+public class 04_arithmetic_shortcuts {
+    public static void main(String[] args) {
+        int count = 0;
+        
+        // Type your code below
+        count += 4;
+        count *= 2;
+        count -= 1;
+        
+        // Don't change the line below
+        System.out.println("count = " + count);
+    }
+}
