9bd52508800b976cef150fffa408911fc3b4e130
igorbdaluz <igorbdaluz@hotmail.com>
2026-05-24 12:46:29 -0300

    Subindo exercícios de operadores

---
+++ Coddy/Fundamentals/4_Operators/05_comparision_operators.java
@@ -0,0 +1,16 @@
+// Escreva um script que inicialize 2 variáveis n1 e n2 com os valores 8 e 9 (respectivamente).
+
+// Depois disso, inicialize outra variável n3 que armazenará se n1 é maior que n2.
+
+public class 05_comparision_operators {
+    public static void main(String[] args) {
+        // Type your code below
+        int n1 = 8;
+        int n2 = 9;
+
+        boolean n3 = n1 > n2;
+        
+        // Don't change the line below
+        System.out.println("n1 = " + n1 + ", n2 = " + n2 + ", n3 = " + n3);
+    }
+}
