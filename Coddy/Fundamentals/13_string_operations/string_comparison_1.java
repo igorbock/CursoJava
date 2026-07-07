e7bb88ea31b3af85d38dd180be6d404bd7f44566
igorbdaluz <igorbdaluz@hotmail.com>
2026-07-06 22:30:11 -0300

    Subindo exercício de comparação de string

--- Coddy/Fundamentals/13_string_operations/string_comparison_1.java
+++ Coddy/Fundamentals/13_string_operations/string_comparison_1.java
@@ -16,7 +16,10 @@ import java.util.Scanner;
 public class string_comparison_1 {
     public static void compareStrings(String str1, String str2) {
         // Write your code here
-        
+        System.out.println(str1 + " equals " + str2 + ": " + str1.equals(str2));
+        System.out.println(str1 + " equalsIgnoreCase " + str2 + ": " + str1.equalsIgnoreCase(str2));
+        System.out.println(str1 + " compareTo " + str2 + ": " + str1.compareTo(str2));
+        System.out.println(str1 + " compareToIgnoreCase " + str2 + ": " + str1.compareToIgnoreCase(str2));
     }
 
     public static void main(String[] args) {
@@ -24,5 +27,6 @@ public class string_comparison_1 {
         String str1 = scanner.nextLine();
         String str2 = scanner.nextLine();
         compareStrings(str1, str2);
+        scanner.close();
     }
 }
